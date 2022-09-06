66e73a2cea2740cbd149b9827fec377079132ccf
tomoropy <ucchi1910@gmail.com>
2022-09-06 23:26:13 +0900

    fix bug 2

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -7,20 +7,20 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
-} from "@mui/material";
-import { ArrowDropUp, Delete, Edit, Save } from "@mui/icons-material";
-import { useState } from "react";
+} from "@mui/material"
+import { ArrowDropUp, Delete, Edit, Save } from "@mui/icons-material"
+import { useState } from "react"
 
 export const ItemList = (props) => {
-  const { todo, setTodo, todoList, setTodoList, editedTodo, setEditedTodo} = props;
-  const [editFlag, setEditFlag] = useState(true);
+  const { todo, todoList, setTodoList, editedTodo, setEditedTodo} = props
+  const [editFlag, setEditFlag] = useState(true)
 
   const handleRemoveTodo = id => {
     const newTodoList = [...todoList]
     setTodoList(newTodoList.filter(todo => !(todo.id === id)))
   }
 
-  const handleUpdateTask = (id) => {
+  const handleUpdateTask = id => {
     const updateTodoList = todoList.map((todo) => {
       if(todo.id === id) todo.isCompleted = !todo.isCompleted 
       return todo
@@ -33,24 +33,26 @@ export const ItemList = (props) => {
     setEditFlag(false)
 }
 
-  const handleEditTodo = e => setEditedTodo(editedTodo => {...editedTodo, body: e.target.value})
+  const handleEditTodo = e => {
+    setEditedTodo({...editedTodo, body: e.target.value})
+  }
 
   const saveTask = () => {
-    if (editedTodo === "") return;
+    if (editedTodo === "") return
     const newTodoList = todoList.map(todo => {
       if(todo.id === editedTodo.id) todo.body = editedTodo.body
       return todo
     })
-    setTodo(newTodoList);
-    setEditFlag(true);
-  };
+    setTodoList(newTodoList)
+    setEditFlag(true)
+  }
 
-  const upTodo = (id) => {
+  const upTodo = id => {
     const newTodoList = [...todoList]
     if(id < 1) return
-    newTodoList.splice(id - 1, 2, newTodoList[id], newTodoList[id - 1]);
+    newTodoList.splice(id - 1, 2, newTodoList[id], newTodoList[id - 1])
     setTodoList(newTodoList)
-  };
+  }
 
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
@@ -89,5 +91,5 @@ export const ItemList = (props) => {
         )}
       </ListItem>
     </List>
-  );
-};
+  )
+}
